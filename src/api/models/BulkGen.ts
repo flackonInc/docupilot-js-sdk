ae7370e9e7e4097b6fbd36c68d45f919813e5862
ravalikab <ravalikab@users.noreply.github.com>
2024-03-28 11:57:16 +0000

    [create-pull-request] automated change

--- src/api/models/BulkGen.ts
+++ src/api/models/BulkGen.ts
@@ -8,7 +8,7 @@ export type BulkGen = {
     readonly id: number;
     readonly errors: Array<BulkGenTask>;
     readonly file_name: string;
-    status?: 0 | 1 | 2 | 3 | 4 | 5;
+    readonly status: 0 | 1 | 2 | 3 | 4 | 5;
     start_time?: string | null;
     end_time?: string | null;
     total?: number | null;
